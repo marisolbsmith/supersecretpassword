0a7e8e4af7477e739f8b7cd4937c79f5befabfe2
mrssmith <marisolbsmith@yahoo.com>
2021-08-14 22:13:47 -0500

    finally

--- Develop/script.js
+++ Develop/script.js
@@ -3,59 +3,92 @@ var generateBtn = document.querySelector("#generate");
 //var
 var upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM".split("");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
-var symbols = "!@#$%^&*".split();
-var numbers = '"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"';
+var symbols = "!@#$%^&*".split("");
+var numbers = "0123456789".split("");
 var passwordLength = upperCase + lowerCase + symbols + numbers;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-// validate
-passwordLength = parseInt(passwordLength);
-upperCase = parseInt(upperCase);
-lowerCase = parseInt(lowerCase);
-numbers = parseInt(numbers);
-symbols = parseInt(symbols);
-
-/*console.log(passwordLength);
-console.log(symbols);
-console.log(numbers);
-console.log(lowerCase);
-console.log(upperCase);*/
-
-//prompts
-function generatePassword() {
+  var password = "";
   var passwordLength = prompt(
     "Enter number of characters in your password, it must be between 8-128 characters?"
   );
-  console.log(passwordLength);
+  if (passwordLength > 128) {
+    window.alert("Didn't I say between 8-128?!");
+    return;
+  }
+  if (passwordLength < 8) {
+    window.alert(
+      "Not enough characters, don't you want your password to be super secret?"
+    );
+    console.log(passwordLength);
+    return;
+  }
+  password += generatePassword(passwordLength);
+  var arrayPassword = password.split("");
+  password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    password += arrayPassword[i];
+  }
+  var passwordText = document.querySelector("#password");
+  console.log(password);
+  passwordText.value = password;
+}
+
+function shuffle(password) {
+  var a = password.split(""),
+    n = a.length;
+
+  for (var i = n - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+  }
+  return a.join("");
+}
+
+//prompts
+function generatePassword(x) {
+  var password = "";
 
   var numbers = confirm("Would you like to use numbers in your password?");
-  console.log(numbers);
+  if (numbers) {
+    for (var i = 0; i < x; i++) {
+      password += getRandomnumbers();
+    }
+  }
 
   var upperCase = confirm("Would you like to include uppercase characters?");
-  console.log(upperCase);
+  if (upperCase) {
+    for (var i = 0; i < x; i++) {
+      password += getRandomupperCase();
+    }
+  }
 
   var lowerCase = confirm("Would you like to include lowercase characters?");
+  if (lowerCase) {
+    for (var i = 0; i < x; i++) {
+      password += getRandomupperCase();
+    }
+  }
   console.log(lowerCase);
 
   var symbols = confirm(
     "Would you like to include symbols AKA special characters?"
   );
+  if (symbols) {
+    for (var i = 0; i < x; i++) {
+      password += getRandomsymbols();
+    }
+  }
   console.log(symbols);
+  var result = shuffle(password);
+  return result;
 }
 
 //alerts
-if (passwordLength > 128) {
-  alert("Didn't I say between 8-128?!");
-}
-if (passwordLength < 8) {
-  alert("Not enough characters, don't you want your password to be super secret?");
-}
+
 //for loop
 for (var i = 0; i <= passwordLength; i++) {
   console.log("My password" + [i] + ".");
@@ -77,7 +110,7 @@ if (symbols === true) {
   console.log(symbols);
 } else console.log("or I will die");
 
-/*function getRandomupperCase() {
+function getRandomupperCase() {
   return upperCase[Math.floor(Math.random() * upperCase.length)];
 }
 console.log(getRandomupperCase);
@@ -90,12 +123,10 @@ console.log(getRandomlowerCase);
 function getRandomnumbers() {
   return numbers[Math.floor(Math.random() * numbers.length)];
 }
-console.log(getRandomnumbers);
 
 function getRandomsymbols() {
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
-console.log(getRandomsymbols);*/
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # supersecretpassword
+
+After long hours of despair and no luck, I somehow put a code together that achieves the objectives. I'm still unsure about some things but the fact that this generater actually generates a password is so exciting and fufilling. Not only does it work but you will indeed enjoy your super secret password!
